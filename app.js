0218ee868fb02ae0f924def81d795a19b17f4ec0
nidhi-vainavi <69414662+nidhi-vainavi@users.noreply.github.com>
2021-03-26 11:00:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+var canvas= document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var ballRadius=10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth)/2;
+
+
+function drawBall (){
+    ctx.beginPath();
+    ctx.arc(x,y,ballRadius,0,Math.PI);
+    ctx.fillStyle="orangered";
+    ctx.fill();
+    ctx.closePath();
+
+
+}
+
+function drawPaddle (){
+    ctx.beginPath();
+    ctx.rect(paddleX,canvas.height-paddleHeight,paddleHeight,paddleWidth);
+    ctx.fillStyle="orangered";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw (){
+    ctx.clearRect(0,0,canvas.width, canvas.height );
+    drawBall();
+    x += dx;
+    y += dy;
+
+    if (y+dy < ballRadius || y + dy>canvas.height - ballRadius){
+        dy = -dy;
+        }
+
+        if (x+dx < ballRadius || x + dx>canvas.width - ballRadius){
+            dx = -dx;
+            }
+}
+
+setInterval(draw,10);
